88d9ace8f4767cc21b4b650a9590cd3426b664bb
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-11-16 12:29:02 +0300

    changement de env

--- public/config.js
+++ public/config.js
@@ -1,6 +1,5 @@
-// public/config.js
 window.__CONFIG__ = {
-  API_BASE_URL: "http://backend:9000/api",
-  MEDIA_URL: "http://backend:9000/media/",
+  API_BASE_URL: "https://api.ecartmada.com/api",
+  MEDIA_URL: "https://api.ecartmada.com/media/",
   APP_NAME: "E.C.A.R.T"
 };
