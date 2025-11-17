dcc626e6234b9fc91ca488c91ef95dac0a1ea74b
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-11-17 17:35:29 +0300

    front

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
-  API_BASE_URL: "https://ecartmada.com/api",
-  MEDIA_URL: "https://ecartmada.com/media/",
+  API_BASE_URL: "https://api.ecartmada.com/api",
+  MEDIA_URL: "https://api.ecartmada.com/media/",
   APP_NAME: "E.C.A.R.T"
 };
