7460ccee6758752e46222707aa9f1177b3b69d4d
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-12-16 04:26:26 +0300

    nouveau

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ import PageDemandesAchat from "./pages/stock/PageDemandesAchat";
 import DepensePage from "./pages/finance/DepensePage";
 import DemandesDecaissement from "./pages/finance/DemandesDecaissement";
 import CreateDecaissement from "./pages/finance/CreateDecaissement";
-import DemandesDisponiblesPage from "./pages/finance/DemandesDisponiblesPage";
+
 
 
 import ValidationsCoordo from "./pages/coordinator/ValidationsCoordo";
@@ -136,7 +136,7 @@ const App = () => (
               <Route path="/finance/depenses" element={<DepensePage />} />
               <Route path="/finance/demandes-decaissement" element={<DemandesDecaissement />} />
               <Route path="/finance/demandes-decaissement/create" element={<CreateDecaissement />} />
-              <Route path="/finance/demandes-disponibles" element={<DemandesDisponiblesPage />} />
+           
 
               <Route path="/cordinator/ValidationsCoordo" element={<ValidationsCoordo />} />
 
